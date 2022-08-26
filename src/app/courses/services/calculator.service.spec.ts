3c272f4d97607fabec8c93cbeded3820f3149192
Josimar Ferreira <josimarferreira922@yahoo.com.br>
2022-08-26 09:16:31 -0300

    Cria teste para CalculatorService

---
+++ src/app/courses/services/calculator.service.spec.ts
@@ -0,0 +1,12 @@
+// O Jasmine é um framework que nos permite testar aplicações JS
+// Ao passo que o Karma é um framework que automatiza nossos testes, implementando o hot reload, por exemplo
+describe("CalculatorService", () => {
+  it("should add two numbers", () => {
+    pending(); //Pending faz com que um desde fique pendente de execução e assim não gere erros, muito usada para quando
+    //queremos implementar um teste no futuro, por exemplo
+  });
+
+  it("should subtract two numbers", () => {
+    pending();
+  });
+});

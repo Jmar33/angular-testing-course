42fefee6a0c21b0a0192f5e630a7c9a60f203668
Josimar Ferreira <josimarferreira922@yahoo.com.br>
2022-09-21 14:41:22 -0300

    Cria teste End2End para lista de cursos avançados

--- cypress/integration/home.test.cy.js
+++ cypress/integration/home.test.cy.js
@@ -1,5 +1,5 @@
 describe("Home Page", () => {
-  it("should display a list of courses ", () => {
+  beforeEach(() => {
     cy.fixture("courses.json").as("coursesJSON");
 
     cy.server();
@@ -7,11 +7,27 @@ describe("Home Page", () => {
     cy.route("/api/courses", "@coursesJSON").as("courses");
 
     cy.visit("/");
+  });
 
+  it("should display a list of courses ", () => {
     cy.contains("All Courses");
 
     cy.wait("@courses");
 
     cy.get("mat-card").should("have.length", 9);
+  });
+
+  it("should display the advanceed courses", () => {
+    cy.get(".mat-tab-label").should("have.length", 2);
+
+    cy.get(".mat-tab-label").last().click();
+
+    cy.get(".mat-tab-body-active .mat-card-title")
+      .its("length")
+      .should("be.gt", 1);
+
+    cy.get(".mat-tab-body-active .mat-card-title")
+      .first()
+      .should("contain", "Angular Security Course");
   });
 });
